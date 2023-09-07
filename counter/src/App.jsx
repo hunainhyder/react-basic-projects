a54e33d43594d9481c98abe6e10c4d95db2ebbf9
Hunain Hyder <65592359+hunainhyder@users.noreply.github.com>
2023-09-07 15:26:28 +0000

    some changes

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -3,21 +3,21 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
-  const [error, setError] = useState("");
+  const [error, setError] = useState(false);
 
   let handleDecrement = () => {
     if (count <= 0) {
-      setError("Cannot decrement below zero");
+      setError(true);
     } else {
-      setError("");
       setCount(count - 1);
     }
   }
-  
+
   return (
     <>
       <h1>Counter is set to {count}</h1>
-      {error}
+      {error ? <div className='danger'><span id='close' onClick={setError(false)}>[&times;]</span><p>Cannot decrement below zero</p></div> : ""}
+      <br />
       <button onClick={handleDecrement}>Decrement</button>
       <button onClick={() => setCount(0)}>Reset</button>
       <button onClick={() => setCount(count + 1)}>Increment</button>
