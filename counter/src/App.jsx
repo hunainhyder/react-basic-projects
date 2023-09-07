58f7193341ae2aedbb016c8886db8208e606a745
Hunain Hyder <65592359+hunainhyder@users.noreply.github.com>
2023-09-07 14:30:19 +0000

    below zero decrement controlled

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -3,11 +3,22 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0);
+  const [error, setError] = useState("");
 
+  let handleDecrement = () => {
+    if (count <= 0) {
+      setError("Cannot decrement below zero");
+    } else {
+      setError("");
+      setCount(count - 1);
+    }
+  }
+  
   return (
     <>
       <h1>Counter is set to {count}</h1>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      {error}
+      <button onClick={handleDecrement}>Decrement</button>
       <button onClick={() => setCount(0)}>Reset</button>
       <button onClick={() => setCount(count + 1)}>Increment</button>
     </>
