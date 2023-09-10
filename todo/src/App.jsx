102285334ea17c9ab06fb10edce5cd8a3afd16fd
Hunain Hyder <65592359+hunainhyder@users.noreply.github.com>
2023-09-10 14:49:58 +0000

    changes in todo

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -5,8 +5,7 @@ import Todo from './Todo';
 function App() {
   let [todos, setTodos] = useState([]);
   let [todo, setTodo] = useState("");
-  let [edit, setEdit] = useState(false);
-  
+
   let deleteTodo = i => {
     let newTodos = [...todos];
 
@@ -14,13 +13,27 @@ function App() {
     setTodos(newTodos);
   }
 
+  let saveTodo = (i, newVal) => {
+    let newTodos = [...todos];
+
+    newTodos.splice(i, 1, newVal);
+    setTodos(newTodos);
+
+    setEdit(false)
+  }
+
+  let handleAddTodo = () => {
+    if (todo.trim() === "") { return; }
+    setTodos([...todos, todo]);
+  }
+
   return (
     <>
       <h1>Todo</h1>
       <input type="text" placeholder="Enter a todo" onChange={e => setTodo(e.target.value)} />
-      <button onClick={() => setTodos([...todos, todo])}>Add Todo</button>
+      <button onClick={handleAddTodo}>Add Todo</button>
       <ul>
-        {todos.map((curr, i) => <Todo key={i} todo={curr} />)}
+        {todos.map((curr, i) => <Todo key={i} todo={curr} funcs={[deleteTodo, saveTodo]} />)}
       </ul>
     </>
   )
--- todo/src/Todo.jsx
+++ todo/src/Todo.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 
-export default function Todo({ i, todo }) {
+export default function Todo({ i, todo, funcs }) {
     let [edit, setEdit] = useState(false);
+    let [newVal, setNewVal] = useState("");
+    let initialValue = todo;
 
     if (edit) {
         return <li key={i}>
-            <input type="text" value={todo} /> 
-            <button onClick={() => deleteTodo(i)}>Delete</button>
-            <button onClick={() => saveTodo(i)}>Save</button>
+            <input type="text" value={initialValue} onChange={e => setNewVal(e.target.value)} /> 
+            <button onClick={() => funcs[0](i)}>Delete</button>
+            <button onClick={() => funcs[1](i, newVal)}>Save</button>
         </li> 
     } else {
         return <li key={i}>
             {todo} 
-            <button onClick={() => deleteTodo(i)}>Delete</button>
+            <button onClick={() => funcs[0](i)}>Delete</button>
             <button onClick={() => setEdit(true)}>Edit</button>
         </li> 
     }
